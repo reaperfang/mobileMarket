e4dbb91f0f1dee5b9fa189e65bcc0e1b13916dc7
chenbaochao <chenbaochao@300.cn>
2019-08-06 21:07:39 +0800

    code

--- src/system/request.js
+++ src/system/request.js
@@ -65,11 +65,7 @@ class Ajax {
     //拼接参数head
     let head = {
       target: config.target,
-<<<<<<< HEAD
       accessToken: store.getters.token || '7834a06f4bcc3d0fc54d7773d5e0149d5e169d161133f8714f906fe8f2d5cc2d',
-=======
-      accessToken: store.getters.token || '7834a06f4bcc3d0fc54d7773d5e0149d3f98de0d0032ba516aad3ae775707115',
->>>>>>> 38d9d04bf2718efeb78b2c7cedc1633c3ebe4965
       client: CONST.CLIENT,
       version: CONST.VERSION,
       requestTime: utils.formatDate(new Date(), "yyyy-MM-dd hh:mm:ss"),
@@ -117,13 +113,10 @@ class Ajax {
           case 'goods': //商品系统
             config.baseURL = `${process.env.DATA_API}/api-commodity-web/commodity/api.do`; // 王浩
           break;
-<<<<<<< HEAD
           case 'order': //订单系统
             config.baseURL = `${process.env.DATA_API}/api-order-web/order/api.do`; // 李刚 尹茂凯
-=======
           case 'decorate':  //装修接口
             config.baseURL = `${process.env.DATA_API}/decoration/api.do`;
->>>>>>> 38d9d04bf2718efeb78b2c7cedc1633c3ebe4965
           break;
         }
       }
