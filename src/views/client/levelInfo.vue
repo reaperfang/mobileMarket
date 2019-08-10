3434118b28dcc2ad0eff056ef79b357bc0caebb9
李可 <like@300.cn>
2019-08-10 17:42:42 +0800

    标签列表页及添加标签页，会员等级，编辑(部分)等级页后台联调及交互

--- src/views/client/levelInfo.vue
+++ src/views/client/levelInfo.vue
@@ -15,7 +15,7 @@
             </el-upload>
             <span class="l_warn fl marL10 marT18">大小不超过2M</span>
         </div>
-        <div class="level_order"><span class="red">*</span>等级序号：VIP1<span class="l_warn">（等级序号为等级在系统的排序，不展示给用户）</span></div>
+        <div class="level_order"><span class="red">*</span>等级序号：{{ levelName }}<span class="l_warn">（等级序号为等级在系统的排序，不展示给用户）</span></div>
         <div class="form_container">
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="right">
                 <el-form-item label="等级称谓：" prop="name">
@@ -30,30 +30,30 @@
                 </el-form-item>
                 <div class="line"></div>
                 <p class="l_title" style="margin-left: -19px;">升级条件：</p><br>
-                <el-form-item>
-                    <el-checkbox v-model="ruleForm.check1">完善信息</el-checkbox>
+                <el-form-item v-if="getIndex(this.conditionList,'完善信息') !== -1">
+                    <el-checkbox v-model="ruleForm.comInfoCheck">完善信息</el-checkbox>
                     <span>已选： 绑定手机号</span>
                     <p style="margin-left:115px;">姓名 <span class="addMainColor marL20" @click="openDialog">选择变更</span></p>
                 </el-form-item>
                 <el-form-item>
-                    <div class="radio_line">
-                        <el-radio v-model="ruleForm.radio1" label="1">消费金额满：</el-radio>
+                    <div class="radio_line" v-if="getIndex(this.conditionList,'消费金额满') !== -1">
+                        <el-radio v-model="ruleForm.comInfoRadio" label="1">消费金额满：</el-radio>
                         <div class="input_wrap2">
                             <el-input placeholder="请输入数字"></el-input>
                         </div>
                         <span>元</span>
                         <span>即可升级</span>
                     </div>
-                    <div class="radio_line">
-                        <el-radio v-model="ruleForm.radio1" label="2">消费次数满：</el-radio>
+                    <div class="radio_line" v-if="getIndex(this.conditionList,'消费次数满') !== -1">
+                        <el-radio v-model="ruleForm.comInfoRadio" label="2">消费次数满：</el-radio>
                         <div class="input_wrap2">
                             <el-input placeholder="请输入数字"></el-input>
                         </div>
                         <span>元</span>
                         <span>即可升级</span>
                     </div>
-                    <div class="radio_line">
-                        <el-radio v-model="ruleForm.radio1" label="2">积分获得满：</el-radio>
+                    <div class="radio_line" v-if="getIndex(this.conditionList,'积分获得满') !== -1">
+                        <el-radio v-model="ruleForm.comInfoRadio" label="3">积分获得满：</el-radio>
                         <div class="input_wrap2">
                             <el-input placeholder="请输入数字"></el-input>
                         </div>
@@ -63,16 +63,16 @@
                 </el-form-item>
                 <div class="line"></div>
                 <p class="l_title" style="margin-left: -19px;">等级权益：</p><br>
-                <el-form-item>
-                    <el-checkbox v-model="ruleForm.check2">满包邮</el-checkbox>
+                <el-form-item v-if="getIndex(this.rightsList,'满包邮') !== -1">
+                    <el-checkbox v-model="ruleForm.rightsCheck1">满包邮</el-checkbox>
                     <span>订单金额满</span>
                     <div class="input_wrap3">
                         <el-input placeholder="请输入数字"></el-input>
                     </div>
                     <span>包邮</span>
                 </el-form-item>
-                <el-form-item>
-                    <el-checkbox v-model="ruleForm.check2">会员折扣</el-checkbox>
+                <el-form-item v-if="getIndex(this.rightsList,'会员折扣') !== -1">
+                    <el-checkbox v-model="ruleForm.rightsCheck2">会员折扣</el-checkbox>
                     <span>享受后买商品售价</span>
                     <div class="input_wrap3">
                         <el-input placeholder="请输入数字"></el-input>
@@ -81,7 +81,7 @@
                     <span class="l_warn">（仅对支仅持参加会员折扣的商品生效）</span>
                 </el-form-item>
                 <div class="line"></div>
-                <p class="l_title" style="margin-left: -19px;">等级权益：</p><br>
+                <p class="l_title" style="margin-left: -19px;">升级奖励：</p><br>
                 <el-form-item>
                     <el-checkbox v-model="ruleForm.check2">赠送积分</el-checkbox>
                     <span>送</span>
@@ -144,6 +144,11 @@ export default {
             ruleForm:{
                 name: "",
                 explain:"",
+                comInfoCheck:"",
+                comInfoRadio:"",
+                rightsCheck1:"",
+                rightsCheck2:"",
+
                 check1: false,
                 radio1: "1",
                 check2: false,
@@ -156,7 +161,10 @@ export default {
             },
             currentDialog:"",
             currentData: '',
-            dialogVisible: false
+            dialogVisible: false,
+            levelName: "",
+            conditionList: [],
+            rightsList: []
         }
     },
     methods: {
@@ -166,7 +174,65 @@ export default {
         openDialog() {
             this.currentDialog = "levelInfoDialog";
             this.dialogVisible = true;
+        },
+        getLevelsInfo(id) {
+            this._apis.client.getLevelsInfo({id:id, cid:""}).then((response) => {
+                this.levelName = response.alias;
+                this.ruleForm = Object.assign({}, response);
+            }).catch((error) => {
+                this.$notify.error({
+                    title: '错误',
+                    message: error
+                });
+            })
+        },
+        getIndex(arr, val) {
+            let i = arr.findIndex((value,index,arr) => {
+                return value.name == val;
+            });
+            return i;
+        },
+        //获取升级条件
+        getLevelCondition() {
+            this._apis.client.levelConditionList({cid: "", type: 0}).then((response) => {
+                let _arr = [];
+                response.map((v) => {
+                    let _obj = {}
+                    _obj.name = v.name;
+                    _obj.id = v.id;
+                    _arr.push(_obj);
+                })
+                this.conditionList = [].concat(_arr);
+            }).catch((error) => {
+                this.$notify.error({
+                    title: '错误',
+                    message: error
+                });
+            })
+        },
+        //获取等级权益
+        getRightsList() {
+            this._apis.client.getRightsList({cid:"", rightsType: 0}).then((response) => {
+                let _arr = [];
+                response.map((v) => {
+                    let _obj = {}
+                    _obj.name = v.name;
+                    _obj.id = v.id;
+                    _arr.push(_obj);
+                })
+                this.rightsList = [].concat(_arr);
+            }).catch((error) => {
+                this.$notify.error({
+                    title: '错误',
+                    message: error
+                });
+            })
         }
+    },
+    mounted() {
+        this.getLevelsInfo(this.$route.query.id);
+        this.getLevelCondition();
+        this.getRightsList();
     }
 }
 </script>
