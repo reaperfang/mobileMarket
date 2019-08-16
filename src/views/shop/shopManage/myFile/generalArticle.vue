a08c3bdb5225285d1c38cb7595372f619a0bef35
sunbilian <sunbilian@300.cn>
2019-08-16 20:36:38 +0800

    店铺我的文件

--- src/views/shop/shopManage/myFile/generalArticle.vue
+++ src/views/shop/shopManage/myFile/generalArticle.vue
@@ -1,17 +1,50 @@
 <template>
-  <p>普通图文
+  <!-- <p>普通图文
     <el-button type="text" @click="dialogVisible=true; currentDialog='dialogSelectImageMaterial'">图片素材</el-button>
     {{ruleForm.coverUrl}}
-    <!-- 动态弹窗 -->
     <component v-if="dialogVisible" :is="currentDialog" :dialogVisible.sync="dialogVisible" @imageSelected="imageSelected"></component>
-  </p>
+  </p> -->
+  <div class="main">
+    <p class="title">创建普通图文</p>
+    <div class="content">
+      <div>
+        <RichEditor @editorValueUpdate="editorValueUpdate" :myConfig="myConfig"></RichEditor>
+      </div>
+      <div class="c_right">
+        <p class="head">图文信息</p>
+        <div class="bodys">
+          <el-input v-model="input" placeholder="标题请勿超过64个字，必填"></el-input>
+          <p class="mt10">封面图片</p>
+          <p class="uploadImage">
+            <el-upload
+              class="upload-demo"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="3"
+              :on-exceed="handleExceed"
+              :file-list="fileList">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb，建议尺寸900*500</div>
+            </el-upload>
+            <el-button type="primary" @click="dialogVisible=true; currentDialog='dialogSelectImageMaterial'" class="imgSource">图片素材</el-button>
+          </p>
+          <el-checkbox v-model="checked" class="mt10">正文中显示封面</el-checkbox>
+        </div>
+      </div>
+    </div>
+    <component v-if="dialogVisible" :is="currentDialog" :dialogVisible.sync="dialogVisible" @imageSelected="imageSelected"></component>
+  </div>
 </template>
 
 <script>
 import dialogSelectImageMaterial from '../../dialogs/dialogSelectImageMaterial';
+import RichEditor from '@/components/RichEditor';
 export default {
   name: 'generalArticle',
-  components: {dialogSelectImageMaterial},
+  components: {dialogSelectImageMaterial, RichEditor},
   data () {
     return {
       dialogVisible: false,
@@ -21,20 +54,91 @@ export default {
         coverUrl: '',
         showCover: true
       },
-      rules: {}
+      rules: {},
+      fileList:[],
+      checked:false,
+      input:'',
+      myConfig: {
+        // 编辑器不自动被内容撑高
+        autoHeightEnabled: false,
+        // 初始容器高度
+        initialFrameHeight: 400,
+        // 初始容器宽度
+        initialFrameWidth: 700,
+        // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
+        serverUrl: 'http://35.201.165.105:8000/controller.php',
+        // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
+        UEDITOR_HOME_URL: '/static/UEditor/'
+      }
     }
   },
   created() {
 
   },
   methods: {
+
+     /* 
+     * 富文本数据更新
+    */
+    editorValueUpdate(value) {
+      this.editorData = value;
+    },
     /* 弹框选中图片 */
     imageSelected(dialogData) {
       this.ruleForm.coverUrl= dialogData.filePath;
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${ file.name }？`);
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.main{
+  background:#fff;
+  padding:20px;
+}
+.title{
+  height: 50px;
+  line-height: 50px;
+}
+.content{
+  display:flex;
+  justify-content:center;
+  .c_right{
+    width: 250px;
+    border:1px solid #e6e6e6;
+    margin-left: 30px;
+    .head{
+      height: 25px;
+      line-height: 25px;
+      background: #EFEFEF;
+      padding-left: 10px;
+    }
+    .bodys{
+      padding: 10px;
+      .uploadImage{
+        position: relative;
+        .imgSource{
+          position: absolute;
+          top:30px;
+          right:10px;
+        }
+      }
+    }
+  }
+}
+.mt10{
+  margin-top: 10px;
+}
 </style>
--- src/views/shop/shopManage/myFile/material/videoMaterial.vue
+++ src/views/shop/shopManage/myFile/material/videoMaterial.vue
@@ -1,25 +1,42 @@
 <template>
-  <!-- <p>视频素材
-    <el-button type="text" @click="dialogVisible=true; currentDialog='dialogUploadVideo'">上传视频</el-button>
-    <component :is="currentDialog" :dialogVisible.sync="dialogVisible"></component>
-  </p> -->
   <div>
     <div class="head">
       <div>
         <el-checkbox v-model="checkedAll">全选</el-checkbox>
-        <el-button type="warning" plain class="ml10">批量删除</el-button>
+        <el-button type="warning" plain class="ml10" @click="deleteImage">批量删除</el-button>
+        <el-button type="warning" plain class="ml10" @click="moveGroup">移动分组</el-button>
       </div>
       <div>
-        <el-button type="primary" plain>移动分组</el-button>
-        <el-button type="primary" plain>同步微信</el-button>
-        <el-button type="primary" plain>上传视频</el-button>
+        <el-button type="primary" plain @click="syncImage">同步微信</el-button>
+        <el-button type="primary" plain @click="uploadImage">上传视频</el-button>
       </div>
     </div>
     <div class="list">
       <p class="list_top">图片素材<span>{{num}}</span>条</p>
       <div class="list_main">
         <div class="list_img">
-           
+           <div class="item_img">
+             <p class="img_head">
+               <span>
+                 <el-checkbox v-model="checked"></el-checkbox>
+                 7月12日
+                </span>
+                <span>
+                  <i class="wx_icon"></i>
+                  8MB
+                </span>
+             </p>
+             <div class="img_body">
+               <p class="title">美丽的天空之城</p>
+               <img src="" class="imgs">
+             </div>
+             <p class="img_bottom">
+               <span><i class="el-icon-edit"></i></span>
+               <span><i class="el-icon-folder"></i></span>
+               <span><i class="el-icon-download"></i></span>
+               <span><i class="el-icon-delete" @click="deleteImage"></i></span>
+             </p>
+           </div>
         </div>
         <div class="groups">
           <p class="groups_head">全部图片</p>
@@ -27,42 +44,69 @@
           <p class="item">
             <span>配图</span>
             <span>
-              <i class="el-icon-edit"></i>
-              <i class="el-icon-delete"></i>
+              <i class="el-icon-edit" @click="newGroup"></i>
+              <i class="el-icon-delete" @click="deleteImage"></i>
             </span>
           </p>
           <p class="item">
             <span>封面</span>
             <span>
-              <i class="el-icon-edit"></i>
-              <i class="el-icon-delete"></i>
+              <i class="el-icon-edit" @click="newGroup"></i>
+              <i class="el-icon-delete" @click="deleteImage"></i>
             </span>
           </p>
-          <span class="newClass">+ 新建分组</span>
+          <span class="newClass" @click="newGroup">+ 新建分组</span>
         </div>
       </div>
     </div>
+    <!-- 动态弹窗 -->
+    <component v-if="dialogVisible" :is="currentDialog" :dialogVisible.sync="dialogVisible"></component>
   </div>
 </template>
 
 <script>
 import dialogUploadVideo from '../../../dialogs/dialogUploadVideo';
+import dialogUploadImage from '../../../dialogs/dialogUploadImage';
+import dialogSync from '../../../dialogs/dialogSync';
+import dialogDelete from '../../../dialogs/dialogDelete';
+import dialogGroups from '../../../dialogs/dialogGroups';
+import dialogGroupsMove from '../../../dialogs/dialogGroupsMove';
 export default {
   name: 'videoMaterial',
-  components: {dialogUploadVideo},
+  components: {dialogUploadVideo,dialogUploadImage,dialogSync,dialogDelete,dialogGroups,dialogGroupsMove},
   data () {
     return {
       dialogVisible: false,
       currentDialog: '',
       checkedAll:false,
       num:10,
+      checked:false
     }
   },
   created() {
 
   },
   methods: {
-
+    newGroup(){
+      this.dialogVisible = true;
+      this.currentDialog = 'dialogGroups'
+    },
+    moveGroup(){
+      this.dialogVisible = true;
+      this.currentDialog = 'dialogGroupsMove'
+    },
+    deleteImage(){
+      this.dialogVisible = true;
+      this.currentDialog = 'dialogDelete'
+    },
+    uploadImage(){
+      this.dialogVisible = true;
+      this.currentDialog = 'dialogUploadImage'
+    },
+    syncImage(){
+      this.dialogVisible = true;
+      this.currentDialog = 'dialogSync'
+    },
   }
 }
 </script>
@@ -85,8 +129,67 @@ export default {
 .list_main{
   display: flex;
   justify-content: space-between;
+  .list_img{
+    display: flex;
+    justify-content: space-around;
+    .item_img{
+      width: 250px;
+      border: 1px solid #e6e6e6;
+      .img_head{
+        height: 25px;
+        line-height: 25px;
+        padding: 0 10px;
+        border-bottom: 1px solid #e6e6e6;
+        display: flex;
+        flex: 1;
+        justify-content: space-between;
+        span{
+          font-size: 14px;
+          color: #92929B;
+        }
+        .wx_icon{
+          width: 20px;
+          height: 20px;
+          display: inline-block;
+          vertical-align: middle;
+          background: url('../../../../../assets/images/shop/wx_icon.png');
+        }
+      }
+      .img_body{
+        width:100%;
+        padding:10px 10px;
+        font-size: 14px;
+        color: #44434B;
+        .title{
+          height: 25px;
+          line-height: 25px;
+        }
+        .imgs{
+          width: 100%;
+          height:85px;
+        }
+      }
+      .img_bottom{
+        height: 25px;
+        line-height: 25px;
+        display: flex;
+        justify-content:space-between;
+        span{
+          width: 25%;
+          display: inline-block;
+          background: #DDDDDD;
+          text-align: center;
+          color: #fff;
+          cursor: pointer;
+        }
+        span:nth-of-type(n+2){
+          border-left:1px solid #fff;
+        }
+      }
+    }
+  }
   .groups{
-    width: 300px;
+    width: 200px;
     border: 1px solid #e6e6e6;
     font-size: 14px;
     color: #44434B;
