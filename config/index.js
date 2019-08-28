f55c6bd3574ef8615ba8eb77e4312efd8bfd68ea
fangyuan <fangyuan@300.cn>
2019-08-28 10:16:29 +0800

    整理接口代理表,删除重复的，单独添加曹帅调试服务
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- config/index.js
+++ config/index.js
@@ -34,54 +34,38 @@ module.exports = {
 
     proxyTable: {
       '/data-server/**': {
-        //target: 'http://172.22.146.2:8080', // 订单 尹茂凯
+        // target: 'http://172.22.146.2:8080', // 订单 尹茂凯
         // target: 'http://172.22.134.106:8080', // 商品 王浩
-        // target: 'http://172.22.146.133:8081',  //李雯廷
-        // target: 'http://www.lwt.com',  //李雯廷2 
-        //target: 'http://www.lwt.com',  //李雯廷2
-        //target: 'http://172.22.134.106:8080', // 商品 王浩
-        // target: 'http://172.22.146.133:8081',  //李雯廷
-        // target: 'http://www.lwt.com',  //李雯廷2
-        // target:'http://172.22.146.124:8099',//许涛 开关
-        //target: 'http://120.133.1.130:8012',//营销
-        //target: 'http://172.22.146.109:8080', // 商品 王浩
-        //target: 'http://172.22.146.31:8080', // 客户 崔建
-        // target: 'http://172.22.146.12:8080', //客户 刘亚凯
+        // target: 'http://172.22.146.133:8081',  // 店铺 李玟霆
+        target: 'http://www.lwt.com',  // nginx代理 李雯廷
+        // target: 'http://172.22.146.124:8099',// 开关 许涛
+        // target: 'http://172.22.146.109:8080', // 商品 王浩
+        // target: 'http://172.22.146.31:8080', // 客户 崔建
+        // target: 'http://172.22.146.12:8080', // 客户 刘亚凯
         // target: 'http://172.22.146.123:8085', // 客户 王杰
-        // target:'https://172.22.146.124:9002',//许涛
-        //target: 'http://172.22.134.125:8012',//陈程
+        // target: 'https://172.22.146.124:9002',// 许涛
+        // target: 'http://172.22.134.125:8012',// 陈程
         // target: 'http://172.22.146.12:8091', // 订单 李刚
-        // target: 'http://172.22.146.133:8081',  //李雯廷
         // target: 'http://172.22.146.114:8080', // 商品 王浩2
-        // target: 'http://172.22.146.133:8081',  //李雯廷
-        target:'http://172.22.146.124:80',//许涛
-        //target: 'http://172.22.146.133:8081',  //李雯廷
-        //target:'http://172.22.146.124:80',//许涛
-        // target: 'http://172.22.146.133:8081',  //李雯廷
-        // target:'http://172.22.146.124:80',//许涛
-        // target:' http://dkmamb.natappfree.cc',//许涛
-        //target: 'http://172.22.134.125:8012',//陈程
-        //target: 'http://172.22.146.12:8091', // 订单 李刚
-        //target: 'http://172.22.146.3:8080', // 订单 张红亮
-        //target: 'http://172.22.146.116:8080', // 商品 王浩
-        //target: 'http://172.22.146.31:8080',
-        //target: 'http://172.22.146.10:8080',
-        // target: 'http://120.133.1.130:8012',//营销测试机
-        //target:'http://172.22.145.115:8080',//耿士龙
-        // target:'http://172.22.145.113:8083',//李权宇
-        //target:'http://172.22.146.119:8080', //郑艳华
-        //target: 'http://172.22.146.3:8080',//张昌超
-        //  target:'http://120.133.1.130:8012',//数据
-        //  target:'http://vc3fhm.natappfree.cc/',//智能运营
+        // target: 'http://172.22.146.124:80',// 许涛
+        // target: 'http://dkmamb.natappfree.cc',// 许涛
+        // target: 'http://172.22.146.3:8080', // 订单 张红亮、张昌超
+        // target: 'http://172.22.146.116:8080', // 商品 王浩
+        // target: 'http://172.22.146.10:8080',
+        // target: 'http://120.133.1.130:8012',// 营销测试机、数据
+        // target: 'http://172.22.145.115:8080',// 耿士龙
+        // target: 'http://172.22.145.113:8083',// 李权宇
+        // target: 'http://172.22.146.119:8080', // 郑艳华 商品
+        // target: 'http://vc3fhm.natappfree.cc/',// 智能运营
         changeOrigin: true,
         pathRewrite: {
           '^/data-server': ''
         }
       },
+
+      /* 营销服务 */
       '/sale_server/**': {
-        // target: 'http://120.133.1.130:8012/',
-        target: 'http://172.22.134.138:8012',  //曹帅
-        // target: 'http://172.22.134.125:8012',  //陈程
+        target: 'http://120.133.1.130:8012/',
         changeOrigin: true,
         pathRewrite: {
           '^/sale_server': ''
@@ -108,7 +92,16 @@ module.exports = {
         pathRewrite: {
           '^/order_server': ''
         }
-      }
+      },
+
+      /* 曹帅接口单独联调 */
+      '/caoshaui_server/**': {
+        target: 'http://172.22.134.138:8012',  //曹帅
+        changeOrigin: true,
+        pathRewrite: {
+          '^/caoshaui_server': ''
+        }
+      },
     }
   },
 
