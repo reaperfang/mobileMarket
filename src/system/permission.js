d8cdb7de7dabf9bb12770dd1f11aa3fe6d80617f
sunbilian <sunbilian@300.cn>
2019-09-09 09:58:21 +0800

    回滚syetem版本

--- src/system/permission.js
+++ src/system/permission.js
@@ -19,7 +19,6 @@ function hasPermission(roles, permissionRoles) {
 
 const whiteList = ['/login', '/auth-redirect']// no redirect whitelist
 
-let flag = 0
 router.beforeEach((to, from, next) => {
   NProgress.start() // start progress bar
   if (true) { // determine if there has token
@@ -29,18 +28,17 @@ router.beforeEach((to, from, next) => {
       next({ path: '/' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
-        if(flag == 0){
-          // let roles = store.getters.shopInfos.data.msfList
-          // let roles = JSON.parse(localStorage.getItem('shopInfos')).data.msfList
-          let roles
-          console.log('functions',JSON.parse(localStorage.getItem('shopInfos')))
+      console.log(store);
+      if (store.getters.roles.length === 0) { // 判断当前用户是否已拉取完user_info信息
+        // store.dispatch('GetUserInfo').then(res => { // 拉取user_info
+          // const roles = res.data.data.roles // note: roles must be a array! such as: ['editor','develop']
+          const roles = ['shop'] // note: roles must be a array! such as: ['editor','develop']
+          store.commit('SET_ROLES', roles)
           store.dispatch('GenerateRoutes', { roles }).then(() => { // 根据roles权限生成可访问的路由表
-            // console.log('addrouter',store.getters.addRouters)
-            router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
-            // next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+            // router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+            router.addRoutes(store.getters.addRouters)
+            next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
           })
-          flag++
-          next()
         // }).catch((err) => {
         //   store.dispatch('FedLogOut').then(() => {
         //     Message.error(err || 'Verification failed, please login again')
@@ -48,13 +46,12 @@ router.beforeEach((to, from, next) => {
         //   })
         // })
       } else {
-        next()
         // 没有动态改变权限的需求可直接next() 删除下方权限判断 ↓
-        // if (hasPermission(store.getters.roles, to.meta.roles)) {
-        //   next()
-        // } else {
-        //   next({ path: '/401', replace: true, query: { noGoBack: true }})
-        // }
+        if (hasPermission(store.getters.roles, to.meta.roles)) {
+          next()
+        } else {
+          next({ path: '/401', replace: true, query: { noGoBack: true }})
+        }
         // 可删 ↑
       }
     }
