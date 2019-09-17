0b074755f6fb3092c8bed031d5c1e72ea1c0a8bc
fangyuan <fangyuan@300.cn>
2019-09-17 18:23:42 +0800

    遗漏的提交

--- src/views/shop/dialogs/dialogUploadImage.vue
+++ src/views/shop/dialogs/dialogUploadImage.vue
@@ -229,7 +229,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .avatar-uploader .el-upload {
   border: 1px dashed #d9d9d9;
   border-radius: 6px;
@@ -248,10 +248,10 @@ export default {
   line-height: 178px;
   text-align: center;
 }
-.avatar {
+/deep/.avatar {
     width: 100%;
     height: 100%;
-    object-fit: cover;
+    object-fit: contain;
     display: block;
 }
 </style>
