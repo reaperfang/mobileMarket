641290cc5997dca4e33c75869381c648010fabb7
fangyuan <fangyuan@300.cn>
2019-09-20 13:30:06 +0800

    修改优惠套餐组件的名称
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- config/index.js
+++ config/index.js
@@ -46,7 +46,7 @@ module.exports = {
 
       /* 营销服务 */
       '/sale_server/**': {
-        target: 'http://120.133.1.130:8012/api/',
+        target: 'http://test-omo.aiyouyi.cn/mkt-api/',
         changeOrigin: true,
         pathRewrite: {
           '^/sale_server': ''
