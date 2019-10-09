38a5277f913044cc41d99f9bacbd27a0aab8c810
chenbaochao <chenbaochao@300.cn>
2019-10-09 16:46:32 +0800

    code

--- src/views/order/dialogs/rejectDialog.vue
+++ src/views/order/dialogs/rejectDialog.vue
@@ -1,8 +1,16 @@
 <template>
     <DialogBase :visible.sync="visible" @submit="submit" :title="title" width="500px" :showFooter="showFooter">
         <div>
-            <el-radio v-model="refuseReason" label="人为破坏拒绝售后">人为破坏拒绝售后</el-radio>
-            <el-radio v-model="refuseReason" label="其他">其他</el-radio>
+            <el-radio v-model="refuseReasonLabel" :label="1">人为破坏拒绝售后</el-radio>
+            <el-radio v-model="refuseReasonLabel" :label="2">其他</el-radio>
+            <el-input
+            style="margin-top: 10px;"
+            v-if="refuseReasonLabel == 2"
+            type="textarea"
+            :rows="2"
+            placeholder="请输入原因"
+            v-model="refuseReason">
+            </el-input>
             <div class="footer">
                 <el-button>取消</el-button>
                 <el-button @click="submit" type="primary">确定</el-button>
@@ -17,12 +25,22 @@ export default {
     data() {
         return {
             showFooter: false,
-            refuseReason: ''
+            refuseReason: '',
+            refuseReasonLabel: 1
         }
     },
     methods: {
         submit() {
-            this.$emit('reject', this.refuseReason)
+            if(this.refuseReasonLabel == 2) {
+                if(!this.refuseReason) {
+                    this.$message({
+                    message: '请输入拒绝原因',
+                    type: 'warning'
+                    });
+                    return
+                }
+            }
+            this.$emit('reject', this.refuseReasonLabel == 1 ? '人为破坏拒绝售后' : this.refuseReason)
             this.visible = false
         }
     },
