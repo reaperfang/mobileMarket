b14248f00f157c31d0751ee563fe9a9d2a32fef4
sunbilian <sunbilian@300.cn>
2019-10-17 10:19:51 +0800

    数据模块bug修改

--- src/views/login/shopsDialog.vue
+++ src/views/login/shopsDialog.vue
@@ -89,26 +89,42 @@ export default {
     justify-content: space-between;
   }
   .content_main{
+    width: 100%;
     display:flex;
-    // justify-content: space-between;
+    flex-wrap: wrap;
     flex: 1;
     margin:20px 0px 30px 0px;
-    div{
+    .shopItem{
       width: 143px;
-      height: 60px;
+      height: 76px;
       border-radius:4px;
       background:rgba(101,94,255,1);
       opacity:0.5;
-      margin-right:10px;
-      span{
-        padding:10px;
-        height: 20px;
-        line-height: 20px;
-        display: block;
-        font-size: 14px;
-        color: #FFFFFF;
-        text-align: left;
-      }
+      margin:0px 10px 10px 0px;
+      cursor: pointer;
+      display:inline-block;
+        span{
+          width: 123px;
+          height:35px;
+          line-height: 25px;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          padding:10px;
+          display: block;
+          font-size: 14px;
+          color: #FFFFFF;
+        }
+
+      // span{
+      //   padding:10px;
+      //   height: 30px;
+      //   line-height: 30px;
+      //   display: block;
+      //   font-size: 14px;
+      //   color: #FFFFFF;
+      //   text-align: left;
+      // }
     }
   }
 }
@@ -121,7 +137,20 @@ export default {
 .dialog_title a:hover{
   text-decoration: underline;
 }
-.shopItem{
-  cursor: pointer;
-}
+// .shopItem{
+//   cursor: pointer;
+//   width:200px;
+//   span{
+//      width: 100%;
+//      height:35px;
+//      line-height: 35px;
+//      overflow: hidden;
+//      white-space: nowrap;
+//      text-overflow: ellipsis;
+//      padding:10px;
+//      display: block;
+//      font-size: 14px;
+//      color: #FFFFFF;
+//   }
+// }
 </style>
