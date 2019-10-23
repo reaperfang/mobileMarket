41287b3c408c28751e67c8ec9ed81aa09d5afefa
chenbaochao <chenbaochao@300.cn>
2019-10-23 09:05:16 +0800

    code

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -64,10 +64,10 @@ export default {
   },
   beforeRouteEnter (to, from, next) {
     next(vm => {
-      if(to.path && !/^\/apply$/.test(to.path)) {
+      if(to.path) {
         console.log(to.path)
-        let name = to.path.replace(/^\/([^\/]+)\/.*$/, '$1')
-        let realCurrent = vm.permission_routers_tree.findIndex(router => router.name == name)
+        let name = to.path.replace(/^(\/[^(?:\/|\?)]+)\/.*$/, '$1')
+        let realCurrent = vm.permission_routers_tree.findIndex(router => router.path == name)
 
         if(realCurrent != vm.current) {
           vm.SETCURRENT(realCurrent)
