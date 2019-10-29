b6667a82ffbdcbfd034693eb1da98f5a94d1ead9
李可 <like@300.cn>
2019-10-29 15:01:32 +0800

    fix jira bug

--- src/views/client/creditsManage.vue
+++ src/views/client/creditsManage.vue
@@ -62,7 +62,7 @@
                     <span class="marR20">积分获取上限</span>
                     <span>每日最高获得：</span>
                     <div class="input_wrap">
-                        <el-input placeholder="请输入整数" v-model="ruleForm.scoreUpperCount" :disabled="!isSwitch" @keyup.native="checkZero($event,ruleForm.scoreUpperCount,'scoreUpperCount')"></el-input>
+                        <el-input placeholder="请输入整数" v-model="ruleForm.scoreUpperCount" :disabled="!isSwitch" @keyup.native="checkZero2($event,ruleForm.scoreUpperCount,'scoreUpperCount')"></el-input>
                     </div>
                     <span>积分</span>
                     <el-button type="primary" class="marL20" v-if="isSwitch" @click="save2">保存</el-button>
@@ -116,6 +116,11 @@ export default {
         },
         checkZero(event,val,ele) {
             val = val.replace(/[^\d.]/g,'');
+            val = val.replace(/^0/g,'');
+            this.ruleForm[ele] = val;
+        },
+        checkZero2(event,val,ele) {
+            val = val.replace(/[^\d]/g,'');
             val = val.replace(/^0/g,'');
             this.ruleForm[ele] = val;
         },
