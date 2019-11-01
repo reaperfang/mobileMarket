7caff446f34cac3dfa96ac752badcfe716ea51eb
李可 <like@300.cn>
2019-11-01 17:55:07 +0800

    同步图文bug

--- src/views/shop/dialogs/dialogSyncArticle.vue
+++ src/views/shop/dialogs/dialogSyncArticle.vue
@@ -113,6 +113,7 @@ export default {
             title:item.title,
             sourceMaterial:item.content,
             isSyncWechat:'1',
+            fileCover: escape(item.thumb_url)
           }
           datas.push(obj)
         }        
