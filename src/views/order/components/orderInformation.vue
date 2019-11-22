5599ebac3da64d0d32f0777c70ea4757c64cd830
李可 <like@300.cn>
2019-11-22 17:14:13 +0800

    补充 bug

--- src/views/order/components/orderInformation.vue
+++ src/views/order/components/orderInformation.vue
@@ -587,7 +587,7 @@ export default {
                 case 3:
                     return '继续发货'
                 case 4:
-                    return '修改收货地址'
+                    return '补填物流信息'
                 case 5:
                     return '发货'
                 case 6:
@@ -598,6 +598,8 @@ export default {
                     return '提前关闭订单'
                 case 9:
                     return '商户备注'
+                case 10:
+                    return '修改收货信息'
             }
         },
         yingshouFilter(val) {
