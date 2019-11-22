b9c61b2f70e7d087396ccf3a66fd77bf7bcbbcc2
李可 <like@300.cn>
2019-11-22 13:39:01 +0800

    CYDSZT-2427

--- src/views/order/components/aftermarketDeliveryInformation.vue
+++ src/views/order/components/aftermarketDeliveryInformation.vue
@@ -81,7 +81,7 @@
                     <div class="header-righter">
                         <div class="header-righter-item">{{orderAfterSale | businessFilter(orderAfterSaleSendInfo.expressNos)}}</div>
                         <div class="header-righter-item">发货人：{{orderAfterSaleSendInfo.sendName}}</div>
-                        <div class="header-righter-item">{{orderAfterSale.receiveGoodsTime}}</div>
+                        <div class="header-righter-item">{{orderAfterSaleSendInfo.sendTime}}</div>
                         <div @click="showContent = !showContent">
                             <i v-if="showContent" class="el-icon-caret-top pointer"></i>
                             <i v-if="!showContent" class="el-icon-caret-bottom pointer"></i>
