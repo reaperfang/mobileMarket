74a68360dcb51a958ffc01d1f37f99746c11e323
sunbilian <sunbilian@300.cn>
2019-12-09 13:44:11 +0800

    修改环境变量

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,9 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	DATA_API: '"https://omo.aiyouyi.cn"',
+	DATA_API: '"http://omo.aiyouyi.cn"',
 	SALE_API: '"/mkt-api"',
 	WEBSOCKET_server:'"ws://127.0.0.1:9000"',
 	staticHost: 'https://test-m-aiyouyi.yun300.cn/',
-	UPLOAD_SERVER:'"https://omo.aiyouyi.cn"',
+	UPLOAD_SERVER:'"http://omo.aiyouyi.cn"',
 }
