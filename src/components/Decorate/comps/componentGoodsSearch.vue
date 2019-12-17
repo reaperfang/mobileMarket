b3b0457eb0a76cf90fa9feee2f2a43e318249b39
fangyuan <fangyuan@300.cn>
2019-12-17 10:44:46 +0800

    搜索框颜色改为固定值
    Signed-off-by: fangyuan <fangyuan@300.cn>

--- src/components/Decorate/comps/componentGoodsSearch.vue
+++ src/components/Decorate/comps/componentGoodsSearch.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- 组件-公用搜索框 -->
     <div class="componentGoodsSearch" style="z-index:4;" :style="[{background:backgroundColor}]" v-if="currentComponentData && currentComponentData.data">
-        <div class="inputBox" :class="[{'textPosition':textPosition!=1},{'borderStyle':borderStyle!=1}]" :style="[{background:borderColor},{height:borderHeight+'px'},{border:'1px solid'+borderColor},{color:fontColor}]">
+        <div class="inputBox" :class="[{'textPosition':textPosition!=1},{'borderStyle':borderStyle!=1}]" :style="[{background:'#eff1f0'},{height:borderHeight+'px'},{border:'1px solid #eff1f0'},{color:fontColor}]">
             <img src="@/assets/images/shop/fdj.png" class="fdj" />
             <p :style="{color:fontColor}">{{hotWords[0]}}</p>
         </div>
