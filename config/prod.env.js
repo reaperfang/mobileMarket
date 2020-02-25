62e3f2c44e8795773c1794076141905c0e2ff27a
sunbilian <sunbilian@300.cn>
2020-02-25 16:27:12 +0800

    清空代码

--- config/prod.env.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-	DATA_API: '"http://omo.aiyouyi.cn"',
-	SALE_API: '"/mkt-api"',
-	WEBSOCKET_server:'"ws://127.0.0.1:9000"',
-	staticHost: 'https://test-m-aiyouyi.yun300.cn/',
-	UPLOAD_SERVER:'"http://omo.aiyouyi.cn"',
-}
